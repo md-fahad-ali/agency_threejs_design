ea1032ec1744752e95b2923dee16935d03456a01
Fahad Ali <141234330+md-fahad-ali@users.noreply.github.com>
2024-10-12 03:35:06 +0600

    update:tada

--- components/Brand.jsx
+++ components/Brand.jsx
@@ -2,11 +2,33 @@
 
 import React, { useEffect, useState } from "react";
 import InfiniteMovingCards from "../ui/infinite-moving-cards";
+import Slider from 'react-infinite-logo-slider'
+import Image from 'next/image'
 
 export default function Brand() {
   return (
     <div className="h-[10rem] rounded-md flex flex-col antialiased bg-transparent items-center justify-center relative overflow-hidden">
-      <InfiniteMovingCards items={testimonials} direction="left" speed="fast" />
+      <Slider
+        width="250px"
+        duration={40}
+        pauseOnHover={true}
+        blurBorders={false}
+        blurBoderColor={'#fff'}
+      >
+        {testimonials?.map((e, i) => (
+          <Slider.Slide key={i}>
+            <div className="relative z-20 mt-6 flex flex-row items-center">
+              <Image
+                src={e?.image}
+                width={100}
+                height={100}
+                alt="Brans pic"
+              />
+            </div>
+          </Slider.Slide>
+        ))}
+      </Slider>
+      {/* <InfiniteMovingCards items={testimonials} direction="left" speed="fast" /> */}
     </div>
   );
 }
